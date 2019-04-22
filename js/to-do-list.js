df72e1e880cbbd94b096b7e1916e4963962879b4
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-04-22 20:58:15 +0300

    added update and delete actions

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,6 +1,6 @@
 window.ToDoList = {
 
-    apiUrl: 'http://localhost:8081',
+    apiUrl: 'http://localhost:8081/to-do-items',
 
     addItem: function () {
 
@@ -22,8 +22,57 @@ window.ToDoList = {
             console.log('Successfully created an item.');
             console.log(response);
 
-            // todo: reload table content
+            ToDoList.getItems();
         });
+    },
+
+    getItems: function () {
+        $.ajax({
+            url: ToDoList.apiUrl,
+            method: "GET"
+        }).done(function (response) {
+            ToDoList.displayItems(JSON.parse(response));
+        });
+    },
+
+    deleteItem: function (id) {
+        $.ajax({
+            url: ToDoList.apiUrl + "?id=" + id,
+            method: "DELETE"
+        }).done(function () {
+            ToDoList.getItems();
+        });
+    },
+
+    markItemDone: function (id, done) {
+        $.ajax({
+            url: ToDoList.apiUrl + "?id=" + id,
+            method: "PUT",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({done: done})
+        }).done(function () {
+            ToDoList.getItems();
+        });
+    },
+
+    displayItems: function (items) {
+        var tableBody = '';
+
+        items.forEach(item => tableBody += ToDoList.getTableRow(item));
+
+        $('#items tbody').html(tableBody)
+    },
+
+    getTableRow: function (item) {
+        let formattedDeadline = new Date(item.deadline).toLocaleDateString("en-US");
+        let checkedAttribute = item.done ? "checked" : "";
+
+        return `<tr>
+        <td class="description">${item.description}</td>
+        <td class="deadline">${formattedDeadline}</td>
+        <td><input type="checkbox" ${checkedAttribute} title="Done" class="mark-done"  data-id=${item.id}></td>
+        <td><a href="#" class="delete fas fa-trash-alt" data-id=${item.id}></a></td>
+    </tr>`
     },
 
     bindEvents: function () {
@@ -36,10 +85,26 @@ window.ToDoList = {
             ToDoList.addItem();
 
             // return false;
-        })
+        });
+
+        $('#items').delegate('.delete', 'click', function () {
+            event.preventDefault();
+
+            let id = $(this).data('id');
+
+            ToDoList.deleteItem(id);
+        });
+
+        $('#items').delegate('.mark-done', 'change', function () {
+            let id = $(this).data('id');
+            let done = $(this).is(":checked");
+
+            ToDoList.markItemDone(id, done);
+        });
 
     }
 };
 
+ToDoList.getItems();
 ToDoList.bindEvents();
 
